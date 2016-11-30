97ef3fa4e8cc425647c48c840526224553f8605f
SHAWN.SH.YANG <SHAWN.SH.YANG@deltaww.com>
2016-11-30 11:21:59 +0800

    1

---
+++ poc-bt/src/main/java/com/shawnyang/poc/bt/PocBtApplication.java
@@ -0,0 +1,12 @@
+package com.shawnyang.poc.bt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocBtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocBtApplication.class, args);
+	}
+}
---
+++ poc-bt/src/test/java/com/shawnyang/poc/bt/PocBtApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shawnyang.poc.bt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PocBtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
